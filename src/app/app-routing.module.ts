458958b4f080928411884a31163c130d637d8d1b
Ihor <igorgorbylin@gmail.com>
2022-09-30 01:31:29 +0300

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SearchPageComponent } from './search-page/search-page.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
+
+const routes: Routes = [
+  { path: 'searching', component: SearchPageComponent },
+  { path: 'details', component: DetailsPageComponent },
+  { path: '', redirectTo: 'searching', pathMatch: 'full' },
+  { path: '**', redirectTo: 'searching', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { SearchingService } from './shared/services/searching.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [SearchingService],
 })
 export class AppComponent {
-  title = 'ngDz7';
+  title = 'OMDB API';
 }
